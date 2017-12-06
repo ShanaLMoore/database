9b70059ce2b7595b2f386e1db637d0dbb4fd4dba
Shana L Moore <shana.lavina.moore@gmail.com>
2017-12-06 13:34:36 -0800

    prototype functions

--- database/database.c
+++ database/database.c
@@ -23,6 +23,7 @@ typedef struct {
     char street[16];
     char city[11];
     char state[2];
+    char zip[5];
     int age;
     char sex[1];
     int tenure;
@@ -32,15 +33,35 @@ typedef struct {
 
 // Declare function prototypes
 int main(void);
+
+// parse input data
 void strsub(char buf[], char sub[], int start, int end);
+
+// read file
 /* int readInfo(Employee workers[]);
+ 
+// output struct
 void outputInfo;
+ 
+// output firt and last name of all men
 void outputMen;
+ 
+// output first and last name of highest paid woman
 void outputTopWoman;
+ 
+// output avg salary
 void outputAvgSalary;
+ 
+// output first and last name of women earning less than avg salary
 void outputBelowAvgWomen;
+ 
+// output ratio of men above avg salary to men below avg
 void outputAboveAvgMen;
+ 
+// output first and last name of all employees who make more than 35k, worked for at least 5yrs, and are over 30
 void outputQuery;
+ 
+// give 10% raise to all who make less than 350/week, output first and last name and new salary of each who received a raise
 void outputRaises; */
 
 int main(void) {
@@ -51,6 +72,8 @@ int main(void) {
     char buf[MAX];
     
     // Open files
+    fpOut = fopen("csis.txt", "w");
+
     if (!(fpIn = fopen("payfile.txt", "r"))){
         printf("payfile.txt could not be opened for input.");
         return(1);
@@ -61,15 +84,20 @@ int main(void) {
             strsub(buf, workers[i].first, 0, 6);
             strsub(buf, workers[i].initial, 8, 8);
             strsub(buf, workers[i].last, 10, 18);
-            i++;
+            strsub(buf, workers[i].street, 20, 35);
+            strsub(buf, workers[i].city, 37, 46);
+            strsub(buf, workers[i].state, 48, 49);
+            strsub(buf, workers[i].zip, 51, 54);
+            strsub(buf, workers[i].age, 56, 59);
+            strsub(buf, workers[i].sex, 61, 61);
+            strsub(buf, workers[i].tenure, 63, 66);
+            strsub(buf, workers[i].salary, 68, 75);
         }
-        
-        printf("%s\n %s\n %s\n", workers[i].first, workers[i].initial, workers[i].last);
-        fprintf(fpOut, "%s\n %s\n %s\n", workers[i].first, workers[i].initial, workers[i].last);
+        puts(buf);
+        //fprintf(fpOut, "%s\n %s\n %s\n", workers[i].first, workers[i].initial, workers[i].last);
 
     }
     
-    fpOut = fopen("csis.txt", "w");
 
     // Perform functions
     
