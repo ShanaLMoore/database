84f4b63410c7f5ebb1a9fb1b6b5103a3c1f90b91
Shana L Moore <shana.lavina.moore@gmail.com>
2017-12-10 12:31:18 -0800

    solution

--- database/database.c
+++ database/database.c
@@ -42,37 +42,39 @@ void strsub(char buf[], char sub[], int start, int end);
 // read file
 int parseWorkerDatabaseFile(FILE * fpIn, Employee workers[]);
 
+// parse file
 void copyWorkerLineToStruct(char* buf, Employee* worker);
 
-
-// output struct
+// output array of structs
 void saveDatabase(FILE * fpOut, int employeeCount, Employee workers[]);
+
+// output all workers + details
 void outputWorker(FILE * fpOut, Employee workers[]);
+
+// output first and last name of all men
 void printMaleWorkers(FILE * fpOut, int employeeCount, Employee workers[]);
-
-
-/*
-// output firt and last name of all men
-void outputMen;
  
 // output first and last name of highest paid woman
-void outputTopWoman;
- 
-// output avg salary
-void outputAvgSalary;
- 
-// output first and last name of women earning less than avg salary
-void outputBelowAvgWomen;
- 
-// output ratio of men above avg salary to men below avg
-void outputAboveAvgMen;
+void printHighestPaidFemale(FILE * fpOut, int employeeCount, Employee workers[]);
+
+// output first and last name of lowest paid man
+void printLowestPaidMale(FILE * fpOut, int employeeCount, Employee workers[]);
+
+// output average salary
+void printAverageSalary(FILE * fpOut, int employeeCount, Employee workers[]);
+
+// output women earning less than average salary
+void printLessThanAvgFemales(FILE * fpOut, int employeeCount, Employee workers[]);
+
+// output ratio of men above/below avg salary
+void printAvgSalaryMaleRatio(FILE * fpOut, int employeeCount, Employee workers[]);
  
 // output first and last name of all employees who make more than 35k, worked for at least 5yrs, and are over 30
-void outputQuery;
- 
+void printQuery(FILE * fpOut, int employeeCount, Employee workers[]);
+
 // give 10% raise to all who make less than 350/week, output first and last name and new salary of each who received a raise
-void outputRaises;
-*/
+void printRaises(FILE * fpOut, int employeeCount, Employee workers[]);
+
 
 int main(void) {
     // Declare & assign variables with appropriate data types
@@ -93,6 +95,13 @@ int main(void) {
     workerCount = parseWorkerDatabaseFile(fpIn, workers);
     saveDatabase(fpOut, workerCount, workers);
     printMaleWorkers(fpOut, workerCount, workers);
+    printHighestPaidFemale(fpOut, workerCount, workers);
+    printLowestPaidMale(fpOut, workerCount, workers);
+    printAverageSalary(fpOut, workerCount, workers);
+    printLessThanAvgFemales(fpOut, workerCount, workers);
+    printAvgSalaryMaleRatio(fpOut, workerCount, workers);
+    printQuery(fpOut, workerCount, workers);
+    printRaises(fpOut, workerCount, workers);
 
     // Close files
     fclose(fpIn);
@@ -111,6 +120,8 @@ void strsub(char buf[], char sub[], int start, int end){
     sub[j] = '\0';
 }
 
+
+// SECTION A
 int parseWorkerDatabaseFile(FILE* fpIn, Employee workers[]){
     char buf[MAX];
     int line = 0;
@@ -147,18 +158,22 @@ void copyWorkerLineToStruct(char* buf, Employee* worker) {
     return;
 }
 
+//SECTION B
 void saveDatabase(FILE * fpOut, int employeeCount, Employee* workers) {
     printf("=== SECTION A: %d WORKERS IMPORTED ===\n", employeeCount);
+    fprintf(fpOut,"=== SECTION A: %d WORKERS IMPORTED ===\n", employeeCount);
 
     printf("\n\n=== SECTION B: ALL WORKERS ===\n\n");
+    fprintf(fpOut,"\n\n=== SECTION B: ALL WORKERS ===\n\n");
 
     for(int i = 0; i < employeeCount; ++i) {
         printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%s\t%d\t%.2lf\n", workers[i].first, workers[i].initial, workers[i].last, workers[i].street, workers[i].city, workers[i].state, workers[i].zip, workers[i].age, workers[i].sex, workers[i].tenure, workers[i].salary);
-        fprintf(fpOut, "%s\t%s\t%s\n", workers[i].first, workers[i].initial, workers[i].last);
+        fprintf(fpOut, "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%s\t%d\t%.2lf\n", workers[i].first, workers[i].initial, workers[i].last, workers[i].street, workers[i].city, workers[i].state, workers[i].zip, workers[i].age, workers[i].sex, workers[i].tenure, workers[i].salary);
     }
     return;
 }
 
+// SECTION C
 void printMaleWorkers(FILE * fpOut, int employeeCount, Employee* workers) {
     printf("\n\n=== SECTION C: MALE WORKERS ===\n\n");
     fprintf(fpOut, "\n\n=== SECTION C: MALE WORKERS ===\n\n");
@@ -173,12 +188,149 @@ void printMaleWorkers(FILE * fpOut, int employeeCount, Employee* workers) {
     return;
 }
 
-/*void outputWorkerName(FILE * fpOut, Employee* worker){
-    printf("\n\n=== SECTION B: MALE WORKERS ===\n\n");
+// SECTION D
+void printHighestPaidFemale(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION D: HIGHEST PAID WOMAN  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION D: HIGHEST PAID WOMAN ===\n\n");
+    double largest = workers[0].salary;
     
-    //  fprint(fpOut, "", ...)
-    printf("%s %s\n", worker->first, worker->last);
-    fprintf(fpOut, "%s %s\n", worker->first, worker->last);
-    return;
-}*/
+    for(int i = 1; i < employeeCount; ++i){
+        
+        if(workers[i].sex[0] == 'F' && largest < workers[i].salary){
+            largest = workers[i].salary;
+        }
+    }
 
+   for(int i = 0; i < employeeCount; ++i) {
+
+        if(workers[i].salary == largest){
+            printf("%s\t%s\n", workers[i].first, workers[i].last);
+            fprintf(fpOut, "%s\t%s\n", workers[i].first, workers[i].last);
+        }
+    }
+    return;
+}
+
+// SECTION E
+void printLowestPaidMale(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION E: LOWEST PAID MAN  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION E: LOWEST PAID MAN ===\n\n");
+    double lowest = workers[0].salary;
+    
+    for(int i = 1; i < employeeCount; ++i){
+        
+        if(workers[i].sex[0] == 'M' && lowest > workers[i].salary){
+            lowest = workers[i].salary;
+        }
+    }
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        
+        if(workers[i].salary == lowest){
+            printf("%s\t%s\n", workers[i].first, workers[i].last);
+            fprintf(fpOut, "%s\t%s\n", workers[i].first, workers[i].last);
+        }
+    }
+    return;
+}
+
+// SECTION F
+void printAverageSalary(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION F: AVERAGE SALARY  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION F: AVERAGE SALARY ===\n\n");
+    double sum = 0;
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        sum += workers[i].salary;
+    }
+    
+    printf("$%.2lf\n", sum/employeeCount);
+    fprintf(fpOut, "$%.2lf\n", sum/employeeCount);
+    return;
+}
+
+// SECTION G
+void printLessThanAvgFemales(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION F: WOMEN EARNING LESS THAN AVG SALARY  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION F: WOMEN EARNING LESS THAN AVG SALARY ===\n\n");
+    double sum = 0;
+    double avgSalary = 0;
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        sum += workers[i].salary;
+    }
+    
+    avgSalary = sum/employeeCount;
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        
+        if(workers[i].sex[0] == 'F' && workers[i].salary < avgSalary){
+            printf("%s\t%s\n", workers[i].first, workers[i].last);
+            fprintf(fpOut, "%s\t%s\n", workers[i].first, workers[i].last);
+        }
+    }
+    
+    return;
+}
+
+// SECTION H
+void printAvgSalaryMaleRatio(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION H: RATIO OF MEN EARNING ABOVE/BELOW AVG SALARY  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION H: RATIO OF MEN EARNING ABOVE/BELOW AVG SALARY ===\n\n");
+    double sum = 0;
+    double avgSalary = 0;
+    float belowAvg = 0;
+    float aboveAvg = 0;
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        sum += workers[i].salary;
+    }
+    
+    avgSalary = sum/employeeCount;
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        if(workers[i].sex[0] == 'M' && workers[i].salary < avgSalary){
+            belowAvg += 1;
+        }
+        if(workers[i].sex[0] == 'M' && workers[i].salary > avgSalary){
+            aboveAvg += 1;
+        }
+    }
+    
+    printf("%.3f : %.3f\n", aboveAvg, belowAvg);
+    fprintf(fpOut, "%.3f : %.3f\n", aboveAvg, belowAvg);
+    
+    return;
+}
+
+// SECTION I
+void printQuery(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION I: EMPLOYEES EARNING OVER $35000/yr, TENURE >= 5yrs, AND AGE > 30  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION I: EMPLOYEES EARNING OVER $35000/yr, TENURE <= 5yrs, AND AGE > 30 ===\n\n");
+   
+    for(int i = 0; i < employeeCount; ++i) {
+        
+        if(((workers[i].salary * 52) > 35000) && (workers[i].tenure >= 5) && (workers[i].age > 30) ){
+            printf("%s\t%s\n", workers[i].first, workers[i].last);
+            fprintf(fpOut, "%s\t%s\n", workers[i].first, workers[i].last);
+        }
+    }
+    return;
+}
+
+// SECTION J
+void printRaises(FILE * fpOut, int employeeCount, Employee* workers) {
+    printf("\n\n=== SECTION J: GIVE 10% RAISES TO THOSE EARNING LESS THAN $350/WK AND PRINT NEW SALARY  ===\n\n");
+    fprintf(fpOut, "\n\n=== SECTION J: GIVE 10% RAISES TO THOSE EARNING LESS THAN $350/WK AND PRINT NEW SALARY ===\n\n");
+    
+    for(int i = 0; i < employeeCount; ++i) {
+        
+        if(workers[i].salary < 350){
+            workers[i].salary += workers[i].salary * .1;
+            
+            printf("%s\t%s\t%.2lf\n", workers[i].first, workers[i].last, workers[i].salary);
+            fprintf(fpOut, "%s\t%s\t%.2lf\n", workers[i].first, workers[i].last, workers[i].salary);
+        }
+    }
+    return;
+}
